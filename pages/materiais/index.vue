6de48eed2dce67c8438de59e26441519b9f25363
vitebo <vitebo51@gmail.com>
2019-04-07 19:18:22 -0300

    list materials

--- pages/materiais/index.vue
+++ pages/materiais/index.vue
@@ -1,6 +1,49 @@
 <template>
   <main class="container">
     <h1>Materiais</h1>
-    <nuxt-link to="/materiais/adicionar">add</nuxt-link>
+    <nuxt-link to="/materiais/adicionar">Adicionar novo material</nuxt-link>
+    <section class="table-responsive-sm">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Tipo</th>
+            <th scope="col">Preço</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="material in materialsSortedByDate" :key="material.id">
+            <th scope="row">{{ material.id }}</th>
+            <td>{{ material.description }}</td>
+            <td>{{ material.unitType }}</td>
+            <td>{{ material.unitPrice }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      materials: [],
+    }
+  },
+  async created() {
+    const { data } = await this.$axios.get('http://localhost:3333/materials/');
+    this.materials = data;
+  },
+  computed: {
+    materialsSortedByDate() {
+      return this.materials.sort((a, b) => {
+        const dateA = new Date(a.updated_at);
+        const dateB = new Date(b.updated_at);
+        return dateA < dateB ? 1 : -1;
+      });
+    }
+  }
+}
+</script>
