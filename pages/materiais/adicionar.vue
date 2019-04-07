e11c97839a8d5b12d6db474b69e2da6cee8cdc15
vitebo <vitebo51@gmail.com>
2019-04-07 19:14:55 -0300

    add material

--- pages/materiais/adicionar.vue
+++ pages/materiais/adicionar.vue
@@ -1,25 +1,25 @@
 <template>
   <main class="container">
     <h1>Materiais</h1>
-    <form @submit="save">
+    <form @submit="addMaterial">
       <fieldset>
         <div class="form-group">
           <label>Descrição</label>
-          <textarea class="form-control" placeholder="Digite a descrição do material" v-model="description">
+          <textarea class="form-control" placeholder="Digite a descrição do material" v-model="material.description">
           </textarea>
         </div>
         <div class="form-group">
           <label>Tipo de unidade</label>
-          <select class="form-control" v-model="unit">
-            <option>Metro</option>
-            <option>Metro quadrado</option>
-            <option>Metro cubico</option>
-            <option>Unitario</option>
+          <select class="form-control" v-model="material.unitType">
+            <option value="metro">Metro</option>
+            <option value="metro quadrado">Metro quadrado</option>
+            <option value="metro cubico">Metro cubico</option>
+            <option value="unitario">Unitario</option>
           </select>
         </div>
         <div class="form-group">
           <label>Preço unitário</label>
-          <input type="text" class="form-control" v-model="price" placeholder="Digite o preço">
+          <input type="text" class="form-control" v-model="material.unitPrice" placeholder="Digite o preço">
         </div>
       </fieldset>
       <button type="submit" class="btn btn-primary">Salvar</button>
@@ -28,25 +28,22 @@
 </template>
 
 <script>
-import FormAddress from '~/components/form_pieces/Address.vue'
-
 export default {
-  components: {
-    FormAddress
-  },
   data() {
     return {
-      description: undefined,
-      unit: 'Metro',
-      price: undefined
+      material: {
+        description: null,
+        unitType: 'metro',
+        unitPrice: null,
+      },
     }
   },
   methods: {
-    save(event) {
+    async addMaterial(event) {
       event.preventDefault();
-      const { description, unit, price } = this;
-      const material = { description, unit, price };
-      window.localStorage.setItem(0, JSON.stringfy(material));
+      const content = this.material;
+      await this.$axios.post('http://localhost:3333/materials/', { content });
+      this.$router.go(-1)
     }
   }
 }
