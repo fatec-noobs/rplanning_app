1b85457e8a2b8a31da06e786fd1d39a734e9a1b8
vitebo <vitebo51@gmail.com>
2019-04-07 14:16:32 -0300

    add service

--- pages/servicos/adicionar.vue
+++ pages/servicos/adicionar.vue
@@ -1,25 +1,25 @@
 <template>
   <main class="container">
     <h1>Serviços</h1>
-    <p>{{ result }}</p>
     <form @submit="addService">
       <fieldset>
         <div class="form-group">
           <label>Descrição</label>
-          <textarea class="form-control" v-model="service.description"></textarea>
+          <textarea class="form-control" v-model="service.description" placeholder="Digite a descrição" required>
+          </textarea>
         </div>
         <div class="form-group">
           <label>Tipo de unidade</label>
-          <select class="form-control" v-model="service.unit_type">
-            <option>Metro</option>
-            <option>Metro quadrado</option>
-            <option>Metro cubico</option>
-            <option>Unitario</option>
+          <select class="form-control" v-model="service.unitType" required>
+            <option value="metro">Metro</option>
+            <option value="metro quadrado">Metro quadrado</option>
+            <option value="metro cubico">Metro cubico</option>
+            <option value="unitario">Unitario</option>
           </select>
         </div>
         <div class="form-group">
           <label>Preço unitário</label>
-          <input type="text" v-model="service.unit_price" class="form-control" placeholder="Digite o preço">
+          <input type="number" v-model="service.unitPrice" class="form-control" placeholder="Digite o preço" required>
         </div>
       </fieldset>
       <button type="submit" class="btn btn-primary">Salvar</button>
@@ -32,9 +32,9 @@ export default {
   data() {
     return {
       service: {
-        description: 'teste',
-        unitType: 'Metro',
-        unitPrice: 10,
+        description: null,
+        unitType: 'metro',
+        unitPrice: null,
       },
       result: null
     }
@@ -42,8 +42,8 @@ export default {
   methods: {
     async addService(event) {
       event.preventDefault();
-      const { data } = await this.$axios.get('http://localhost:3333/');
-      this.result = data.greeting;
+      const content = this.service;
+      await this.$axios.post('http://localhost:3333/services/', { content });
     }
   }
 }
