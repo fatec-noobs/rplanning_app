731f093af910e27127c12675f89f3e169d6235df
vitebo <andre.vitebo@redealumni.com>
2019-04-21 16:48:52 -0300

    add address

--- pages/clientes/adicionar.vue
+++ pages/clientes/adicionar.vue
@@ -27,6 +27,36 @@
           <input type="text" class="form-control" placeholder="Digite o seu cpf" v-model="client.cpf">
         </div>
       </fieldset>
+      <fieldset>
+        <div class="form-group">
+          <label>CEP</label>
+          <input type="text" class="form-control" placeholder="Digite o seu CEP" v-model="address.cep">
+        </div>
+        <div class="form-group">
+          <label>Logradouro</label>
+          <input type="text" class="form-control" placeholder="Digite o seu logradouro" v-model="address.publicPlace">
+        </div>
+        <div class="form-group">
+          <label>Número</label>
+          <input type="text" class="form-control" placeholder="Digite o seu número" v-model="address.number">
+        </div>
+        <div class="form-group">
+          <label>Complemento</label>
+          <input type="text" class="form-control" placeholder="Digite o seu complemento" v-model="address.complement">
+        </div>
+        <div class="form-group">
+          <label>Bairro</label>
+          <input type="text" class="form-control" placeholder="Digite o seu bairro" v-model="address.neighborhood">
+        </div>
+        <div class="form-group">
+          <label>Cidade</label>
+          <input type="text" class="form-control" placeholder="Digite a sua cidade" v-model="address.city">
+        </div>
+        <div class="form-group">
+          <label>Estado</label>
+          <input type="text" class="form-control" placeholder="Digite o seu estado" v-model="address.state">
+        </div>
+      </fieldset>
       <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
   </main>
@@ -45,8 +75,17 @@ export default {
         name: null,
         email: null,
         phone: null,
-        type: 'fisica',
+        type: null,
         cpf: null,
+      },
+      address: {
+        cep: null,
+        publicPlace: null,
+        number: null,
+        complement: null,
+        neighborhood: null,
+        city: null,
+        state: null,
       },
     }
   },
@@ -54,6 +93,7 @@ export default {
     async addClient(event) {
       event.preventDefault();
       const content = this.client;
+      content.address = this.address;
       await this.$axios.post('http://localhost:3333/clients/', { content });
       this.$router.go(-1);
     }
