547407f694744f0bd9b888520162974f1d07dd48
vitebo <vitebo51@gmail.com>
2019-05-11 16:56:20 -0300

    add budget

--- pages/orcamentos/adicionar.vue
+++ pages/orcamentos/adicionar.vue
@@ -1,37 +1,48 @@
 <template>
   <main class="container">
     <h1>Orçamentos</h1>
-    <form @submit="addService">
+    <form @submit="saveBudget">
 
       <fieldset>
-        <div class="form-group">
-          <label>Serviços</label>
-          <select class="form-control" v-model="service.unit_type" required>
-            <option v-for="service in services" value="{{ service.value }}">{{ service.name }}</option>
+        <legend>Materiais</legend>
+        
+        <div class="form-group" v-for="budget_material, index in budgets_materials" >
+          <label>Material {{ index + 1 }}</label>
+          <select class="form-control" v-model="budget_material.value" required>
+            <option v-for="material in materials_type" :value="material.id">{{ material.description }}</option>
           </select>
+          <input v-model="budget_material.amount" class="form-control" type="number" placeholder="quantidade"/>
         </div>
 
-        <button class="btn btn-primary">Adicionar mais um</button>
+        <button class="btn btn-secondary" @click="removeMaterial">Remover um</button>
+        <button class="btn btn-primary" @click="addMaterial">Adicionar mais um</button>
+      </fieldset>
+
+      <fieldset>
+        <legend>Serviços</legend>
+        
+        <div class="form-group" v-for="budget_service, index in budgets_services">
+          <label>Serviço {{ index + 1 }}</label>
+          <select class="form-control" v-model="budget_service.value" required>
+            <option v-for="service in services_type" :value="service.id">{{ service.description }}</option>
+          </select>
+          <input v-model="budget_service.amount" class="form-control" type="number" placeholder="quantidade"/>
+        </div>
+
+        <button class="btn btn-secondary" @click="removeService">Remover um</button>
+        <button class="btn btn-primary" @click="addService">Adicionar mais um</button>
       </fieldset>
 
       <fieldset>
         <div class="form-group">
-          <label>Materiais</label>
-          <select class="form-control" v-model="service.unit_type" required>
-            <option v-for="material in materials" value="{{ material.value }}">{{ material.name }}</option>
+          <label>Cliente</label>
+          <select class="form-control" v-model="budget_client.id" required>
+            <option v-for="client in client_list" :value="client.id">{{ client.name }}</option>
           </select>
         </div>
       </fieldset>
 
-      <fieldset>
-        <div class="form-group">
-          <label>Clientes</label>
-          <select class="form-control" v-model="service.unit_type" required>
-            <option v-for="client in clients" value="{{ client.value }}">{{ client.name }}</option>
-          </select>
-        </div>
-      </fieldset>
-
+      <button class="btn btn-primary">Salvar</button>
     </form>
   </main>
 </template>
@@ -40,9 +51,51 @@
 export default {
   data() {
     return {
-      service: [],
-      materials: [],
-      clients: [],
+      budgets_services: [ { service_id: null, amount: 1 } ],
+      budgets_materials: [ { material_id: null, amount: 1 } ],
+
+      services_type: [],
+      materials_type: [],
+
+      client_list: [],
+      budget_client: { id: null }
+    }
   },
+  async created() {
+    const serices_type = await this.$axios.get('http://localhost:3333/services/');
+    this.serices_type = serices_type.data;
+
+    const materials_type = await this.$axios.get('http://localhost:3333/materials/');
+    this.materials_type = materials_type.data;
+
+    const client_list = await this.$axios.get('http://localhost:3333/clients/');
+    this.client_list = client_list.data;
+  },
+  methods: {
+    addMaterial() {
+      this.budgets_materials.push({ material_id: null, amount: 1 })
+    },
+    removeMaterial() {
+      this.budgets_materials.pop(-1)
+    },
+    addService() {
+      this.budgets_services.push({ service_id: null, amount: 1 })
+    },
+    removeService() {
+      this.budgets_services.pop(-1)
+    },
+    calcValue() {
+
+    },
+    async saveBudget(event) {
+      event.preventDefault();
+      const content = {
+        client_id: budget_client.id,
+
+      }
+      await this.$axios.post('http://localhost:3333/materials/', { content });
+      this.$router.go(-1)
+    }
+  }
 }
 </script>
